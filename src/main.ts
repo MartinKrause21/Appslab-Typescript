24a77903bd06baada5fdd30ff2449dfbc98ab2e9
MartinKrause21 <martin.21.krause@gmail.com>
2021-10-05 09:11:00 +0200

    check if int numbers are triangle

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,13 @@
-        let a: number = 5;
-        let b: number = 5;
-        let c: number = 4;
-        if (((a == b)  && (b == c))) {
-            console.log(3);
-        }
-        else if (((a == b) || ((b == c) || (a == c)))) {
-            console.log(2);
-        }
-        else {
-            console.log(0);
-        }
+let a: number = 2;
+let b: number = 3;
+let c: number = 4;  
+
+if (a + b > c && b + c > a && a + c > b ) {
+    console.log(true);
+}
+
+else {
+    console.log(false);
+}
         
     
