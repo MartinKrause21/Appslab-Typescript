e1e4b1809a4596409152af62dcbe9538c9f2df8a
MartinKrause21 <martin.21.krause@gmail.com>
2021-10-06 21:08:24 +0200

    check if is the end of string same

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,8 @@
-function checkEnding(a: string, b: string) {
+function checkEnding(a: string, b: string){
     return a.endsWith(b);
-  
   }
   console.log(checkEnding("green", "bluee"));
-
   interface String {    
     endsWith(searchString: string, endPosition?: number): boolean;
+
 };
